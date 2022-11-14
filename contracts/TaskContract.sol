9a3f1661aff106d23eb51cc3ffbbb5f6c5107132
OmarMWarraich <earthling986@gmail.com>
2022-11-14 18:53:11 +0500

    feat: Delete and Fetch Task Functionality

--- contracts/TaskContract.sol
+++ contracts/TaskContract.sol
@@ -23,6 +23,29 @@ contract TaskContract {
         taskToOwner[taskId] = msg.sender;
         emit AddTask(msg.sender, taskId);
     }
+
+    function deleteTask(uint _taskId) external {
+        require(taskToOwner[_taskId] == msg.sender, "You are not the owner of this task");
+        tasks[_taskId].isDeleted = true;
+        emit DeleteTask(_taskId, true);
+    }
+
+    function getMyTasks() external view returns (Task[] memory){
+        Task[] memory temporary = new Task[](tasks.length);
+        uint counter = 0;
+        for (uint i = 0; i < tasks.length; i++) {
+            if (taskToOwner[i] == msg.sender && tasks[i].isDeleted == false) {
+                temporary[counter] = tasks[i];
+                counter++;
+            }
+        }
+        Task[] memory result = new Task[](counter);
+        for (uint i = 0; i < counter; i++) {
+            result[i] = temporary[i];
+        }
+        return result;
+    }
+
 }
 
 
